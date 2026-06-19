e3bc347cbe2e5d41495f8416c3daf850b3dc4f39
Nisel <nisel11good@gmail.com>
2026-06-19 22:30:08 +0300

    nix: Use local instead of remote repo

--- shell.nix
+++ shell.nix
@@ -17,13 +17,8 @@ let
 
   lkpatcher = python3Packages.buildPythonPackage rec {
     pname = "lkpatcher";
-    version = "master";
-    src = fetchFromGitHub {
-      owner = "R0rt1z2";
-      repo = "lkpatcher";
-      rev = "master";
-      sha256 = "1s77wnm1jc3cgy7xmjicvdc9294qfg0vijfxp8mp3hwgd9cqkjcs";
-    };
+    version = "local";
+    src = ./.;
 
     pyproject = true;
     build-system = [ python3Packages.setuptools ];
